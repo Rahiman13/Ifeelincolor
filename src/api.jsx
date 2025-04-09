b34764c6839b009e2ea00cf24026fc1f3a9f009a
Rahiman Shaik <rahimanshaik13@gmail.com>
2025-04-09 17:00:42 +0530

    api-vps

--- src/api.jsx
+++ src/api.jsx
@@ -1,5 +1,6 @@
 // const BaseUrl='http://localhost:3000'
 // const BaseUrl='https://rough-1-gcic.onrender.com'
-const BaseUrl='http://18.209.44.54:3000'
+// const BaseUrl='http://18.209.44.54:3000'
+const BaseUrl='https://Ifeelincolorvps.projexino.com'
 
 export default BaseUrl;
